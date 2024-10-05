995d5ac35d5d2d65b11ea1e299c243881c680601
Andy Ng <andysstuff@andys-mbp.mynetworksettings.com>
2024-10-04 21:06:55 -0400

    comments

--- data/tests/test_people.py
+++ data/tests/test_people.py
@@ -7,6 +7,9 @@ def test_get_people():
     assert len(people) > 0
     # Check for string IDs:
     for _id, person in people.items():
+        # id represents the key and person represents the value of each dict item
+        #people.items() returns a list of dictionary key-val pairs
         assert isinstance(_id, str)
+        # checks if NAME is a key in the person (value)
         assert ppl.NAME in person
     
