8f7b761ced0ea5c079ddd8863a9ca5a7a450ee81
Andy Ng <andysstuff@10-18-135-58.dynapool.wireless.nyu.edu>
2024-10-17 17:21:40 -0400

    Linting Updates for test_people.py

--- data/tests/test_people.py
+++ data/tests/test_people.py
@@ -8,13 +8,14 @@ def test_read():
     assert len(people) > 0
     # Check for string IDs:
     for _id, person in people.items():
-        # id represents the key and person represents the value of each dict item
-        #people.items() returns a list of dictionary key-val pairs
+        # id represents the key and person represents
+        # the value of each dict item
+        # people.items() returns a list of dictionary key-val pairs
         assert isinstance(_id, str)
         # checks if NAME is a key in the person (value)
         assert ppl.NAME in person
-    
-    
+
+
 def test_delete():
     people = ppl.read()
     assert isinstance(people, dict)
@@ -22,25 +23,28 @@ def test_delete():
     ppl.delete(ppl.DEL_EMAIL)
     people = ppl.read()
     # checks if new people dict has length smaller than old
-    assert len(people) < old_len 
+    assert len(people) < old_len
     # make sure the email deleted is still not in people dict
     assert ppl.DEL_EMAIL not in people
 
 
-#email is made constant here to make it easier to change later on
+# email is made constant here to make it easier to change later on
 ADD_EMAIL = 'joe@nyu.edu'
 
-#testing the create endpoint
+
+# testing the create endpoint
 def test_create():
-    #creates the person
+    # creates the person
     ppl.create('Joe Smith', 'NYU', ADD_EMAIL)
     people = ppl.read()
-    #checks if ADD_EMAIL is in people already
+    # checks if ADD_EMAIL is in people already
     assert ADD_EMAIL in people
 
-#testing the delete people endpoint
+
+# testing the delete people endpoint
 def test_create_duplicate():
-    #checks if the email already exists after creating the person
-    #if so, it will raise an error
+    # checks if the email already exists after creating the person
+    # if so, it will raise an error
     with pytest.raises(ValueError):
-        ppl.create('Name Does Not matter', 'Neither Does School', ppl.TEST_EMAIL)
+        ppl.create('Name Does Not matter',
+                   'Neither Does School', ppl.TEST_EMAIL)
