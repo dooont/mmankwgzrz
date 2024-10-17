1a239794c40c0ae3ec4b2289988e629bae13a801
gzhao10 <zgavin73@gmail.com>
2024-10-17 11:53:04 -0400

    Added read() to text.py

--- data/text.py
+++ data/text.py
@@ -25,3 +25,20 @@ text_dict = {
         TEXT: 'This is a text to delete.',
     },
 }
+
+def read():
+    """
+    Our contract:
+        - No arguments.
+        - Returns a dictionary of users keyed on user email.
+        - Each user email must be the key for another dictionary.
+    """
+    text = text_dict
+    return text
+
+
+def main():
+    print(read())
+
+if __name__ == '__main__':
+    main()
