e00c1e7695db3382bd14036a5be91a4aea64320f
gzhao10 <zgavin73@gmail.com>
2024-10-24 11:50:40 -0400

    added test_read_one, test_read_one_not_found

--- data/tests/test_text.py
+++ data/tests/test_text.py
@@ -7,3 +7,13 @@ def test_read():
     # check if each page is a str
     for key in texts:
         assert isinstance(key, str)
+
+
+def test_read_one():
+    # check that TEST_KEY is being read properly
+    assert len(txt.read_one(txt.TEST_KEY)) > 0
+
+
+def test_read_one_not_found():
+    # check that non-existent pages can't be read
+    assert txt.read_one('Not a page key!') == {}
