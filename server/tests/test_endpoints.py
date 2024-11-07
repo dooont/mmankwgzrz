2557b266308be909cc49222f79b5720929ed4101
gzhao10 <zgavin73@gmail.com>
2024-11-07 12:48:25 -0500

    fancier testing for test_read

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -45,6 +45,18 @@ def test_get_repo_name():
     assert isinstance(resp_json[ep.REPO_NAME_RESP], str)
     assert len(resp_json[ep.REPO_NAME_RESP]) > 0
 
+# testing the test_read function from people
+@patch('data.people.read', autospec=True,
+       return_value={'id': {NAME: 'Joe Schmoe'}})
+def test_read(mock_read):
+    resp = TEST_CLIENT.get(ep.PEOPLE_EP)
+    assert resp.status_code == OK
+    resp_json = resp.get_json()
+    for _id, person in resp_json.items():
+        assert isinstance(_id, str)
+        assert len(_id) > 0
+        assert NAME in person
+
 
 # testing the people endpoint
 def test_get_people():
