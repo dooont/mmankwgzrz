2f098310300bc6ffbd941d6c74b0181e6f3c7d3c
Kelly <kellywu18029@gmail.com>
2024-11-08 14:44:39 -0500

    added fixtures in test_roles.py

--- data/tests/test_roles.py
+++ data/tests/test_roles.py
@@ -1,18 +1,28 @@
+import pytest
 import data.roles as rls
 
 
-def test_get_roles():
-    roles = rls.get_roles()
-    assert isinstance(roles, dict)
-    assert len(roles) > 0
-    for code, role in roles.items():
+@pytest.fixture
+def roles_data():
+    return rls.get_roles()
+
+
+@pytest.fixture
+def masthead_roles_data():
+    return rls.get_masthead_roles()
+
+
+def test_get_roles(roles_data):
+    assert isinstance(roles_data, dict)
+    assert len(roles_data) > 0
+    for code, role in roles_data.items():
         assert isinstance(code, str)
         assert isinstance(role, str)
 
 
-def test_get_masthead_roles():
-    mh_roles = rls.get_masthead_roles()
-    assert isinstance(mh_roles, dict)
+def test_get_masthead_roles(masthead_roles_data):
+    assert isinstance(masthead_roles_data, dict)
+    assert set(masthead_roles_data.keys()).issubset(rls.MH_ROLES)
 
 
 def test_get_role_codes():
@@ -24,3 +34,4 @@ def test_get_role_codes():
 
 def test_is_valid():
     assert rls.is_valid(rls.TEST_CODE)
+    assert not rls.is_valid("NOT A VALID ROLE")
