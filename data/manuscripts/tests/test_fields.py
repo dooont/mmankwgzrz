69af9bca4f908adfa7d9a7244f1e5848006c506f
dooont <matthewmaung@QQs-Laptop>
2024-11-14 14:34:19 -0500

    added test_get_fld_names and test_get_disp_name in test_fields.py for fields.py

--- data/manuscripts/tests/test_fields.py
+++ data/manuscripts/tests/test_fields.py
@@ -3,3 +3,15 @@ import data.manuscripts.fields as mflds
 
 def test_get_flds():
     assert isinstance(mflds.get_flds(), dict)
+
+
+def test_get_fld_names():
+    ret = list(mflds.get_fld_names())
+    assert isinstance(ret, list)
+    assert mflds.TITLE in ret
+
+
+def test_get_disp_name():
+    ret = mflds.get_disp_name(mflds.TITLE)
+    assert isinstance(ret, str)
+    assert ret == mflds.TEST_FLD_DISP_NM
