95b6eb1bfb555c147839b246328973517fafd518
gzhao10 <zgavin73@gmail.com>
2024-11-19 12:12:48 -0500

    renaming in db_connect.py

--- data/db_connect.py
+++ data/db_connect.py
@@ -5,7 +5,7 @@ import pymongo as pm
 LOCAL = "0"
 CLOUD = "1"
 
-GAME_DB = 'gamesDB'
+SE_DB = 'seDB'
 
 client = None
 
@@ -38,7 +38,7 @@ def connect_db():
     return client
 
 
-def insert_one(collection, doc, db=GAME_DB):
+def create(collection, doc, db=SE_DB):
     """
     Insert a single doc into collection.
     """
@@ -46,7 +46,7 @@ def insert_one(collection, doc, db=GAME_DB):
     return client[db][collection].insert_one(doc)
 
 
-def fetch_one(collection, filt, db=GAME_DB):
+def fetch_one(collection, filt, db=SE_DB):
     """
     Find with a filter and return on the first doc found.
     Return None if not found.
@@ -58,26 +58,26 @@ def fetch_one(collection, filt, db=GAME_DB):
         return doc
 
 
-def del_one(collection, filt, db=GAME_DB):
+def del_one(collection, filt, db=SE_DB):
     """
     Find with a filter and return on the first doc found.
     """
     client[db][collection].delete_one(filt)
 
 
-def update_doc(collection, filters, update_dict, db=GAME_DB):
+def update_doc(collection, filters, update_dict, db=SE_DB):
     return client[db][collection].update_one(filters, {'$set': update_dict})
 
 
 # might be unnecessary
-def fetch(collection, db=GAME_DB):
+def fetch(collection, db=SE_DB):
     ret = []
     for doc in client[db][collection].find():
         ret.append(doc)
     return ret
 
 
-def read(collection, db=GAME_DB, no_id=True) -> list:
+def read(collection, db=SE_DB, no_id=True) -> list:
     """
     Returns a list from the db.
     """
@@ -85,11 +85,11 @@ def read(collection, db=GAME_DB, no_id=True) -> list:
     for doc in client[db][collection].find():
         if no_id:
             del doc[MONGO_ID]
-            ret.append(doc)
+        ret.append(doc)
     return ret
 
 
-def read_dict(collection, key, db=GAME_DB, no_id=True) -> dict:
+def read_dict(collection, key, db=SE_DB, no_id=True) -> dict:
     recs = read(collection, db=db, no_id=no_id)
     recs_as_dict = {}
     for rec in recs:
@@ -97,7 +97,7 @@ def read_dict(collection, key, db=GAME_DB, no_id=True) -> dict:
     return recs_as_dict
 
 
-def fetch_all_as_dict(key, collection, db=GAME_DB):
+def fetch_all_as_dict(key, collection, db=SE_DB):
     ret = {}
     for doc in client[db][collection].find():
         del doc[MONGO_ID]
