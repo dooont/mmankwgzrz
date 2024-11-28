0129e84620fd1f5e90c9ba02c1365af9398f7038
Andy Ng <ngandy0202@gmail.com>
2024-11-27 23:39:00 -0500

    Added test_form testing functions

--- data/manuscripts/tests/test_form.py
+++ data/manuscripts/tests/test_form.py
@@ -1,17 +1,19 @@
-# import data.manuscripts.form as mform
+import data.manuscripts.form as mform
 
 
-# def test_get_form():
-#     assert isinstance(mform.get_form(), list)
+def test_get_form():
+    assert isinstance(mform.get_form(), list)
 
 
-# def test_get_form_descr():
-#     form_descr = mform.get_form_descr()
-#     for item in form_descr:
-#         assert 'description' in item
+def test_get_form_descr():
+    form_descr = mform.get_form_descr()
+    assert isinstance(form_descr, dict)
+    for item in form_descr:
+        assert isinstance(item, str )
+        assert 'username' or 'password' in item
 
 
-# def test_get_fld_names():
-#     fld_names = mform.get_fld_names()
-#     assert isinstance(fld_names, list)
-#     assert all(isinstance(name, str) for name in fld_names)
+def test_get_fld_names():
+    fld_names = mform.get_fld_names()
+    assert isinstance(fld_names, list)
+    assert all(isinstance(name, str) for name in fld_names)
