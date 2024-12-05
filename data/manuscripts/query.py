6bac54916031cfbfbf272ff4caa9ab169f23e03d
Andy Ng <ngandy0202@gmail.com>
2024-12-05 01:58:07 -0500

    Added state table

--- data/manuscripts/query.py
+++ data/manuscripts/query.py
@@ -60,6 +60,32 @@ def is_valid_action(action: str) -> bool:
 FUNC = 'f'
 
 STATE_TABLE = {
+    AUTHOR_REVIEW: {},
+    AUTHOR_REVISION: {
+        ACTIONS['DONE']: {
+            FUNC: lambda x: EDITOR_REVIEW,
+        }
+    },
+    COPY_EDIT: {},
+    EDITOR_REVIEW: {
+        ACTIONS['ACCEPT']: {
+            FUNC: lambda x: COPY_EDIT,
+        }
+    },
+    FORMATTING: {},
+    PUBLISHED: {},
+    REFEREE_REVIEW: {
+        ACTIONS['ACCEPT']: {
+            FUNC: lambda x: COPY_EDIT,
+        },
+        ACTIONS['REJECT']: {
+            FUNC: lambda x: REJECTED,
+        },
+        ACTIONS['ACCEPT_WITH_REV']: {
+            FUNC: lambda x: AUTHOR_REVISION,
+        }
+    }, 
+    REJECTED: {},
     SUBMITTED: {
         ACTIONS['ASSIGN_REF']: {
             FUNC: lambda x: REFEREE_REVIEW,
@@ -68,32 +94,46 @@ STATE_TABLE = {
             FUNC: lambda x: REJECTED,
         }
     },
+    WITHDRAWN: {},
 }
 
 
 def handle_action(curr_state, action) -> str:
-    if not is_valid_state(curr_state):
+    # if not is_valid_state(curr_state):
+    #     raise ValueError(f'Invalid state: {curr_state}')
+    # if not is_valid_action(action):
+    #     raise ValueError(f'Invalid action: {action}')
+    if curr_state not in STATE_TABLE:
         raise ValueError(f'Invalid state: {curr_state}')
-    if not is_valid_action(action):
+    if action not in STATE_TABLE[curr_state]:
         raise ValueError(f'Invalid action: {action}')
-    new_state = curr_state
-    if curr_state == SUBMITTED:
-        if action == ACTIONS['ASSIGN_REF']:
-            new_state = REFEREE_REVIEW
-        elif action == ACTIONS['REJECT']:
-            new_state = REJECTED
-    elif curr_state == REFEREE_REVIEW:
-        if action == ACTIONS['ACCEPT']:
-            new_state = COPY_EDIT
-        elif action == ACTIONS['REJECT']:
-            new_state = REJECTED
-        elif action == ACTIONS['ACCEPT_WITH_REV']:
-            new_state = AUTHOR_REVISION
-    elif curr_state == AUTHOR_REVISION:
-        if action == ACTIONS['DONE']:
-            new_state = EDITOR_REVIEW
-    elif curr_state == EDITOR_REVIEW:
-        if action == ACTIONS['ACCEPT']:
-            new_state = COPY_EDIT
+    # if curr_state == SUBMITTED:
+    #     if action == ACTIONS['ASSIGN_REF']:
+    #         new_state = REFEREE_REVIEW
+    #     elif action == ACTIONS['REJECT']:
+    #         new_state = REJECTED
+    # elif curr_state == REFEREE_REVIEW:
+    #     if action == ACTIONS['ACCEPT']:
+    #         new_state = COPY_EDIT
+    #     elif action == ACTIONS['REJECT']:
+    #         new_state = REJECTED
+    #     elif action == ACTIONS['ACCEPT_WITH_REV']:
+    #         new_state = AUTHOR_REVISION
+    # elif curr_state == AUTHOR_REVISION:
+    #     if action == ACTIONS['DONE']:
+    #         new_state = EDITOR_REVIEW
+    # elif curr_state == EDITOR_REVIEW:
+    #     if action == ACTIONS['ACCEPT']:
+    #         new_state = COPY_EDIT
         
-    return new_state
+    return STATE_TABLE[curr_state][action][FUNC](x=curr_state)
+    
+    
+
+
+def main():
+    print(handle_action(SUBMITTED, ACTIONS['ASSIGN_REF']))
+
+
+if __name__ == '__main__':
+    main()
