db109bd25f7f8dc946b822154bb811f2bd6b9d80
Andy Ng <ngandy0202@gmail.com>
2024-12-10 12:29:00 -0500

    Created Sample Manu and Common Actions

--- data/manuscripts/query.py
+++ data/manuscripts/query.py
@@ -1,3 +1,5 @@
+from manuscripts import fields as flds
+
 # States
 AUTHOR_REVIEW = 'AU_RVW'
 AUTHOR_REVISION = 'AU_REV'
@@ -25,6 +27,11 @@ VALID_STATES = [
     WITHDRAWN,
 ]
 
+SAMPLE_MANU = {
+    flds.TITLE: 'Sample Manuscript',
+    flds.AUTHOR: 'Andy Ng',
+    flds.REFEREES: [],
+}
 
 def get_states() -> list[str]:
     return VALID_STATES
@@ -60,64 +67,71 @@ def is_valid_action(action: str) -> bool:
 
 FUNC = 'f'
 
+COMMON_ACTIONS = {
+    ACTIONS['WITHDRAW']: {
+        FUNC: lambda **kwargs: WITHDRAWN,
+    },
+}
+
 STATE_TABLE = {
     AUTHOR_REVIEW: {
         ACTIONS['DONE']: {
-            FUNC: lambda x: FORMATTING,
+            FUNC: lambda **kwargs: FORMATTING,
         }
     },
     AUTHOR_REVISION: {
         ACTIONS['DONE']: {
-            FUNC: lambda x: EDITOR_REVIEW,
+            FUNC: lambda **kwargs: EDITOR_REVIEW,
         }
     },
     COPY_EDIT: {
         ACTIONS['DONE']: {
-            FUNC: lambda x: AUTHOR_REVIEW,
+            FUNC: lambda **kwargs: AUTHOR_REVIEW,
         }
     },
     EDITOR_REVIEW: {
         ACTIONS['ACCEPT']: {
-            FUNC: lambda x: COPY_EDIT,
+            FUNC: lambda **kwargs: COPY_EDIT,
         }
     },
     FORMATTING: {
         ACTIONS['DONE']: {
-            FUNC: lambda x: PUBLISHED,
+            FUNC: lambda **kwargs: PUBLISHED,
         }
     },
     PUBLISHED: {
         ACTIONS['DONE']: {
-            FUNC: lambda x: PUBLISHED,
+            FUNC: lambda **kwargs: PUBLISHED,
         },
     },
     REFEREE_REVIEW: {
         ACTIONS['ACCEPT']: {
-            FUNC: lambda x: COPY_EDIT,
+            FUNC: lambda **kwargs: COPY_EDIT,
         },
         ACTIONS['REJECT']: {
-            FUNC: lambda x: REJECTED,
+            FUNC: lambda **kwargs: REJECTED,
         },
         ACTIONS['ACCEPT_WITH_REV']: {
-            FUNC: lambda x: AUTHOR_REVISION,
+            FUNC: lambda **kwargs: AUTHOR_REVISION,
         }
     }, 
     REJECTED: {
         ACTIONS['DONE']: {
-            FUNC: lambda x: REJECTED,
+            FUNC: lambda **kwargs: REJECTED,
         }    
     },
     SUBMITTED: {
         ACTIONS['ASSIGN_REF']: {
-            FUNC: lambda x: REFEREE_REVIEW,
+            FUNC: lambda **kwargs: REFEREE_REVIEW,
         },
         ACTIONS['REJECT']: {
-            FUNC: lambda x: REJECTED,
-        }
+            FUNC: lambda **kwargs: REJECTED,
+        },
+        **COMMON_ACTIONS
     },
     WITHDRAWN: {
         ACTIONS['WITHDRAW']: {
-            FUNC: lambda x: WITHDRAWN,
+            FUNC: lambda **kwargs: WITHDRAWN,
         }
     },
 }
@@ -127,19 +141,19 @@ def get_valid_actions_by_state(state: str) -> list:
     return list(STATE_TABLE[state].keys())
                 
 
-def handle_action(curr_state, action) -> str:
+def handle_action(curr_state, action, **kwargs) -> str:
     if curr_state not in STATE_TABLE:
         raise ValueError(f'Invalid state: {curr_state}')
     if action not in STATE_TABLE[curr_state]:
         raise ValueError(f'Invalid action: {action}')
 
-    return STATE_TABLE[curr_state][action][FUNC](x=curr_state)
+    return STATE_TABLE[curr_state][action][FUNC](**kwargs)
     
     
 
 
 def main():
-    print(handle_action(SUBMITTED, ACTIONS['ASSIGN_REF']))
+    print(handle_action(SUBMITTED, ACTIONS['ASSIGN_REF'], manu=SAMPLE_MANU))
 
 
 if __name__ == '__main__':
